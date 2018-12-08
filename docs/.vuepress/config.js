7a46ffd8b4692aff0beee2a4b3d6339970921dd7
brownhu <1104524352@qq.com>
2018-12-08 11:12:51 +0800

    博客重新搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'Brown Hu',
+  description: 'Replace the stars and rivers in the body with unlimited methods for the future.',
+  head: [
+    ['link', { rel: 'icon', href: '/hero  .jpg' }]
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: 'Blog',
+        items: [
+          { text: 'JavaScript', link: '/Blog/JavaScript/' },
+          { text: 'CSS', link: '/Blog/CSS/' },
+          { text: 'HTTP', link: '/Blog/HTTP/' },
+          { text: 'Library', link: '/Blog/Library/' },
+          { text: 'Other', link: '/Blog/Other/' },
+          { text: 'Wheel', link: '/Blog/Wheel/' },
+        ]
+      },
+      { text: 'Project', link: '/Project/' },
+      { text: 'Resume', link: '/Resume/' },
+      { text: 'GitHub', link: 'https://google.com' },
+    ]
+  }
+}
---
+++ docs/Resume/README.md
@@ -0,0 +1,25 @@
+# 胡秉亮
+
+## 基本信息
+- 应聘职位：前端开发工程师
+- 手机：16602620124
+- Email：1104524352@qq.com
+- [Github](https://github.com/hubingliang)
+- [个人网站/技术博客](http://brownhu.site)
+
+## 项目介绍
+
+[移至Project](/Project/)
+
+## 技术文章
+
+- [学会了ES6，就不会写出那样的代码](https://juejin.im/post/5acb1847f265da237c693362) 掘金800+
+- [Hexo已经看腻了，来试试VuePress搭建个人博客](https://juejin.im/post/5addb90af265da0b7f442935) 掘金400+
+
+## 掌握技能
+
+熟悉Vue（2.5+）相关技术栈，在实际项目中有Vuex和Vue-router的使用经验，对Vue响应式原理有一定认识。
+
+对HTML语义化有一定理解，并在项目构建中以ES6+为主，以Less预处理CSS，已经能够在实际项目中运用TypeScript与vue结合的方式编写代码，正在学习Rxjs。
+
+掌握VW+PostCSS解决移动端适配问题。
