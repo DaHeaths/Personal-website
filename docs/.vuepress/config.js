26fc2b35c308d488502890b0110bfcb144b4ed38
Brownhu <1104524352@qq.com>
2019-04-18 19:44:40 +0800

    博客更新 19-4-18

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,61 +1,45 @@
 module.exports = {
-  title: 'Brown Hu',
-  description: 'Replace the stars and rivers in the body with unlimited methods for the future.',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.png' }]
-  ],
-  themeConfig: {
-    nav: [
-      {
-        text: 'Blog',
-        items: [
-          { text: 'JavaScript', link: '/JavaScript/' },
-          { text: 'CSS', link: '/CSS/' },
-          { text: 'HTTP', link: '/HTTP/' },
-          { text: 'Library', link: '/Library/' },
-          { text: 'Wheel', link: '/Wheel/' },
-        ]
-      },
-      { text: 'Project', link: '/Project/' },
-      { text: 'Resume', link: '/Resume/' },
-      { text: 'GitHub', link: 'https://github.com/hubingliang' },
-    ],
-    sidebar: {
-      '/JavaScript/': [
-        '',
-        'shallow-copy',
-        'sort',
-        'event-model',
-        'inherit',
-        'this',
-        'event-commission',
-        'let&const',
-        'qqq',
-        'requestAnimationFrame',
-        'Browser',
-        // 'deep-copy',
-        'type'
-      ],
-      '/CSS/': [
-        '',
-        '47css-trick',
-        'rem',
-        'clear-float'
-      ],
-      '/HTTP/': [
-        '',
-        'security',
-        'caching',
-        'status-code'
-      ],
-      '/Library/': [
-        '',
-        'emoji'
-      ],
-      '/Wheel/': [
-        ''
-      ]
-    }
-  },
-  base: '/'
+    title: 'Brown Hu',
+    description: 'Replace the stars and rivers in the body with unlimited methods for the future.',
+    head: [['link', { rel: 'icon', href: '/favicon.png' }]],
+    themeConfig: {
+        nav: [
+            {
+                text: 'Blog',
+                items: [
+                    { text: 'JavaScript', link: '/JavaScript/' },
+                    { text: 'CSS', link: '/CSS/' },
+                    { text: 'HTTP', link: '/HTTP/' },
+                    { text: 'Library', link: '/Library/' },
+                    { text: 'Wheel', link: '/Wheel/' },
+                ],
+            },
+            { text: 'Project', link: '/Project/' },
+            { text: 'Resume', link: '/Resume/' },
+            { text: 'GitHub', link: 'https://github.com/hubingliang' },
+        ],
+        sidebar: {
+            '/JavaScript/': [
+                '',
+                'shallow-copy',
+                'sort',
+                'event-model',
+                'inherit',
+                'this',
+                'event-commission',
+                'let&const',
+                'qqq',
+                'requestAnimationFrame',
+                'Browser',
+                // 'deep-copy',
+                'type',
+            ],
+            '/CSS/': ['', '47css-trick', 'rem', 'clear-float'],
+            '/Vue/': [''],
+            '/HTTP/': ['', 'security', 'caching', 'status-code', 'XDomain'],
+            '/Library/': ['', 'emoji', 'vscode-theme'],
+            '/Wheel/': [''],
+        },
+    },
+    base: '/',
 }
